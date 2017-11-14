e096b7f0d6b3b3d235fd00321cf08cb4621e8b55
Harry Baines <harrybaines@Harrys-MacBook-Pro.local>
2017-11-14 18:34:14 +0000

    Able to read byte array from file at given offset

--- coursework/src/Driver.java
+++ coursework/src/Driver.java
@@ -11,8 +11,10 @@ public class Driver {
 
 	public Driver() {
 
+        System.out.println(ByteOrder.nativeOrder());
+
 		try {
-			file = new RandomAccessFile("hello.txt", "r");
+			file = new RandomAccessFile("ext2fs", "r");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}  
@@ -20,16 +22,22 @@ public class Driver {
         // Read text file into byte array
         try {
 
-            System.out.println(ByteOrder.nativeOrder());
-
             // Array of 8-bit signed 2s complement integers 
             fileInBytes = new byte[(int)file.length()];
             file.readFully(fileInBytes);
 
-            ByteBuffer.wrap(fileInBytes);
+            // Wrap existing byte array to byte buffer
+            ByteBuffer byteBuffer = ByteBuffer.wrap(fileInBytes);
+            
+            byte[] byteArray = new byte[2048];
 
-            for (byte b : fileInBytes)
-                System.out.print(b + " "); 
+            byteBuffer.get(byteArray, 0, 2048);
+
+            this.read(1023, 1024);
+
+            // for (byte b : byteArray)
+            //     System.out.println(b);
+
             System.out.print("\n");
 
         } catch (IOException e) {
@@ -37,6 +45,32 @@ public class Driver {
         }
 
 	}
+
+    /**
+     * Reads at most length bytes starting at byte offset startByte from start of file.
+     * Byte 0 is first byte in the file.
+     *
+     * @param startByte The byte to begin reading from in the file.
+     * @param length The total number of bytes to read.
+     * @return The array of bytes from the file.
+     */
+    private byte[] read(long startByte, long length) {
+
+        byte[] specifiedBytes = new byte[(int)length];
+
+        int offset = (int) startByte;
+
+        for (int curByte = 0; curByte < length; curByte++) {
+
+            offset++;
+            System.out.println("Reading byte: " + curByte + " at byte offset: " + offset);
+            specifiedBytes[curByte] = fileInBytes[(int)offset];
+            System.out.println(specifiedBytes[curByte]);
+
+        }
+
+        return specifiedBytes;
+    } 
 
     /**
      * Main method to begin the driver program.
